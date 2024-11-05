1f06f331e5026d86b7b9dcee8c9c7c9f1e01f8c3
ZuzannaTokarczyk <133810453+ZuzannaTokarczyk@users.noreply.github.com>
2024-11-05 11:19:44 +0100

    Update k_means.py

--- k_means.py
+++ k_means.py
@@ -3,23 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 
-def update_assignments(data, centroids):
-    assignments = []
-    for point in data:
-        distances = np.linalg.norm(point - centroids, axis=1) 
-        assignments.append(np.argmin(distances))  
-    return assignments
-
-def update_centroids(data, num_clusters, assignments):
-    centroids = []
-    for c in range(num_clusters):
-        cluster_points = data[np.array(assignments) == c] 
-        if len(cluster_points) > 0:
-            centroids.append(np.mean(cluster_points, axis=0))  
-        else:
-            centroids.append(np.random.random(data.shape[1]))  
-    return np.array(centroids)
-
 data, labels = load_digits(return_X_y=True)
 (n_samples, n_features), n_digits = data.shape, np.unique(labels).size
 print(f"# digits: {n_digits}; # samples: {n_samples}; # features {n_features}")
