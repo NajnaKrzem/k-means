ced6620c835b920c7fbd69890e0a8db4f121e64b
ZuzannaTokarczyk <133810453+ZuzannaTokarczyk@users.noreply.github.com>
2024-11-05 11:28:03 +0100

    Create unittest.py

---
+++ testy/unittest.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestKMeansFunctions(unittest.TestCase):                                               
+
+    def test_update_assignments(self):                                                      
+        data = np.array([[1, 2], [2, 3], [3, 4]])
+        centroids = np.array([[1, 2], [4, 5]])
+        assignments = update_assignments(data, centroids)
+        expected_assignments = [0, 0, 1]  # Oczekiwane przypisania
+        self.assertEqual(assignments, expected_assignments)
+
+    def test_update_centroids(self):
+        data = np.array([[1, 2], [2, 3], [3, 4]])                                           
+        assignments = [0, 0, 1]  # Przypisania do klastrów
+        new_centroids = update_centroids(data, 2, assignments)                              
+        expected_centroids = np.array([[1.5, 2.5], [3, 4]])  
+        np.testing.assert_array_almost_equal(new_centroids, expected_centroids)
+
+if __name__ == '__main__':
+    unittest.main() 
+
+'''if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False) '''
